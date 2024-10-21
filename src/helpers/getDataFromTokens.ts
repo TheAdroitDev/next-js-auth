934191d69f46aa0def545ca3846dd4656f6c42e8
TheAdroitDev <starshivamverma2@gmail.com>
2024-10-21 10:18:52 +0530

    Update getDataFromTokens.ts

--- src/helpers/getDataFromTokens.ts
+++ src/helpers/getDataFromTokens.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 export const getDataFromTokens = (request:NextRequest) => {
